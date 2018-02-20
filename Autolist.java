ed2b2525fb8ac80ba95f9696ccf8224858e86971
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2018-02-20 23:35:56 +0200

    Add files via upload

---
+++ Autolist.java
@@ -0,0 +1,16 @@
+package Cars;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Autolist {
+	
+	public Autolist() {
+		Auto.class;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}

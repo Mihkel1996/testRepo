e0a313f7dd316f52f585b3d99d85dc3a75b4b133
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2018-02-20 23:20:43 +0200

    Update Rooms.java

--- Rooms.java
+++ Rooms.java
@@ -1,17 +1,80 @@
-package Classroom;
+package Cars;
 
-public class Rooms {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-	public static void main(String[] args) {
-		Rooms ruuminumber;
-		Rooms korrus;
-		Rooms ruumiliik;
-		Rooms �pper�hmad;
-		
-		
-		//public Rooms(String ruuminumber, int korrus) {
-			
-		}
+public class Auto {
 
-	}
+		private String mark;
+		private String mudel;
+		private String numbrimärk;
+		private Date kuupäev;
+		private List<String> omanikud;
+
+public Auto(String automark, String mudel, String numbrimärk, Date kuupäev, List<String> omanikud) {
+	this.mark = automark;
+	this.mudel = mudel;
+	this.numbrimärk = numbrimärk;
+	this.setKuupäev(kuupäev);
+	this.omanikud = new ArrayList<>();
+}
+
+/**
+ * @return the automark
+ */
+public String getAutomark() {
+	return mark;
+}
+
+/**
+ * @param automark the automark to set
+ */
+public void setAutomark(String automark) {
+	this.mark = automark;
+}
+
+/**
+ * @return the mudel
+ */
+public String getMudel() {
+	return mudel;
+}
+
+/**
+ * @param mudel the mudel to set
+ */
+public void setMudel(String mudel) {
+	this.mudel = mudel;
+}
+
+/**
+ * @return the numbrimärk
+ */
+public String getNumbrimärk() {
+	return numbrimärk;
+}
+
+/**
+ * @param numbrimärk the numbrimärk to set
+ */
+public void setNumbrimärk(String numbrimärk) {
+	this.numbrimärk = numbrimärk;
+}
+
+public void addOmanikud(String omanik) {
+	this.omanikud.add(omanik);
+}
+
+public Date getKuupäev() {
+	return kuupäev;
+}
+
+public void setKuupäev(Date kuupäev) {
+	this.kuupäev = kuupäev;
+}
+
+
+}
+
 

7d809eb80e36d1f5e4cbda1710f29fbaac9c7e2b
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2018-02-21 06:42:40 +0200

    Update Autolist.java

--- Autolist.java
+++ Autolist.java
@@ -1,13 +1,15 @@
-package Cars;
+ackage Cars;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 public class Autolist {
 	
-	public Autolist() {
-		Auto.class;
-	}
+		public Auto(String firstname, String lastname) {
+			this.firstname = firstname;
+			this.lastname = lastname;
+		}
+		// audi1.addOwner(“Kati Murakas”
 
 	public static void main(String[] args) {
 		

2ceb3a478b0f5d0c3b3c7310ccf508b784493425
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2018-03-07 03:58:53 +0200

    Update Mudelid.java

--- Mudelid.java
+++ Mudelid.java
@@ -1,9 +1,22 @@
 
+import java.util.List;
+import LocalDateTime;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Mudelid {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String nimetus;
+	private Tootmisalguskuupäev alguskuupäev;
+	private Tootmislõppkuupäev lõppkuupäev;
+	private String mudeliliik;
 
+	public Mudelid(String nimetus, Tootmisalguskuupäev alguskuupäev, Tootmislõppkuupäev lõppkuupäev,
+			Mudeliliik mudeliliik) {
+		this.nimetus = nimetus;
+		this.alguskuupäev = alguskuupäev;
+		this.lõppkuupäev = lõppkuupäev;
+		this.mudeliliik = mudeliliik;
 	}
 
 }

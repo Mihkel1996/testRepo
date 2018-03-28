e8f4a525a968989f02526c84bd8fc2512a0fd040
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2018-03-28 15:02:25 +0300

    Update Autolist.java

--- Autolist.java
+++ Autolist.java
@@ -11,7 +11,27 @@ public class Autolist {
 		List<String> omanikud = new ArrayList<>(Arrays.asList("Juri", "Raivo"));
 		Car a7 = new Car(Car.Mark.AUDI, new Model("A7", "2011 - 2017", Model.Type.SEDAN),
                 "6H7DD", LocalDate.of(2012, 4, 9));
-		System.out.println(a7);
+		Car a8 = new Car(Car.Mark.BMW, new Model("A8", "2011 - 2017", Model.Type.SEDAN),
+				"6H7DD", LocalDate.of(2012, 4, 9));
+		Car a9 = new Car(Car.Mark.FORD, new Model("A7", "2011 - 2017", Model.Type.SEDAN),
+				"6H7DD", LocalDate.of(2012, 4, 9));
+		
+		List<Car> autod = new ArrayList<>();
+		autod.add(a7);
+		autod.add(a8);
+		autod.add(a9);
+		
+		List<Car> audid = new ArrayList<>();
+		for (Car auto : autod) {
+			if (auto.getMark() == Car.Mark.AUDI) {
+				audid.add(auto);
+			}
+		}
+		
+		
+		
+		System.out.println(autod);
+		System.out.println(audid);
 		
 	}
 
